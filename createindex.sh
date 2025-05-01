6c1c77ccae9de9022032eaab675b6406daab63ac
rlucente <rlucente@comcast.net>
2025-04-30 20:55:14 -0400

    fix list of examples

--- createindex.sh
+++ createindex.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-find -s $(dirname $0) -name '*.asm' -exec head -1 {} \; -print | sed 's/\(; ..*\)/[\1](/g;N;s/\n//g;s/$/?plaintext=1)/g;s/^\[; /*[/g'
+find -s $(dirname $0) -name '*.asm' -exec head -1 {} \; -print | sed 's/\(; ..*\)/[\1](/g;N;s/\n//g;s/$/?plaintext=1)/g;s/^\[; /* [/g'
