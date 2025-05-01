913c59290ab6486126b1347126f0639b14bc2e98
rlucente <rlucente@comcast.net>
2025-04-30 21:00:20 -0400

    comments and formatting cleanup

--- createindex.sh
+++ createindex.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-find -s $(dirname $0) -name '*.asm' -exec head -1 {} \; -print | sed 's/\(; ..*\)/[\1](/g;N;s/\n//g;s/$/?plaintext=1)/g;s/^\[; /* [/g'
+# This script creates the index of all of the examples and problem
+# solutions at the end of the README.md file.
+
+find -s $(dirname $0) -name '*.asm' -exec head -1 {} \; -print | \
+    sed 's/\(; ..*\)/[\1](/g;N;s/\n//g;s/$/?plaintext=1)/g;s/^\[; /* [/g'
