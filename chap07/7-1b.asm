f5906d0fb3c84bd0e28c3aee0e42f15d1a0b309e
rlucente <rlucente@comcast.net>
2025-05-07 09:31:53 -0400

    uses bcd method for hex to ascii

--- chap07/7-1b.asm
+++ chap07/7-1b.asm
@@ -1,9 +1,9 @@
-; 7-1. hexadecimal to ascii
+; 7-1b. hexadecimal to ascii
 
 ; Convert the contents of memory location $40 to an ASCII character.
 ; Memory location $40 contains a single hexadecimal digit (the four
 ; most significant bits are zero). Store the ASCII character in memory
-; location $41.
+; location $41. This uses alternate BCD method with no branching.
 
         org     $4000
         setdp   0
