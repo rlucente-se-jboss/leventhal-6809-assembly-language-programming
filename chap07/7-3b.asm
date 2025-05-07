d7c617e9cdb37432b0bdd49845fd90b9e8135b85
rlucente <rlucente@comcast.net>
2025-05-07 09:20:41 -0400

    ascii to hexadecimal

--- chap07/7-3b.asm
+++ chap07/7-3b.asm
@@ -1,9 +1,10 @@
-; 7-3b. ascii to decimal
+; 7-3b. ascii to hexadecimal
 
 ; Convert the contents of memory location $40 from an ASCII character
-; to a decimal digit and store the result in memory location $41. If
-; the contents of memory location $40 are not the ASCII representation
-; of a decimal digit, set the contents of memory location $41 to $FF.
+; to a hexadecimal digit and store the result in memory location $41.
+; If the contents of memory location $40 are not the ASCII representation
+; of a hexadecimal digit, set the contents of memory location $41 to
+; $FF.
 
         org     $4000
         setdp   0
@@ -27,15 +28,15 @@ data    fcb     '7'
 result  fcb     0
 
 ; 30 = 0 00
-; 31 = 0 01
-; 32 = 0 02
-; 33 = 0 03
-; 34 = 0 04
-; 35 = 0 05
-; 36 = 0 06
-; 37 = 0 07
-; 38 = 0 08
-; 39 = 0 09
+; 31 = 1 01
+; 32 = 2 02
+; 33 = 3 03
+; 34 = 4 04
+; 35 = 5 05
+; 36 = 6 06
+; 37 = 7 07
+; 38 = 8 08
+; 39 = 9 09
 ; 3a = X 0a
 ; 3b = X 0b
 ; 3c = X 0c
@@ -43,9 +44,9 @@ result  fcb     0
 ; 3e = X 0e
 ; 3f = X 0f
 ; 40 = X 10
-; 41 = A 11 00
-; 42 = B 12 01
-; 43 = C 13 02
-; 44 = D 14 03
-; 45 = E 15 04
-; 46 = F 16 05
+; 41 = A 11 00  0a
+; 42 = B 12 01  0b
+; 43 = C 13 02  0c
+; 44 = D 14 03  0d
+; 45 = E 15 04  0e
+; 46 = F 16 05  0f
